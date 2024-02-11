ccc9cb5acb41fb66d6f4539718b3eb649e164fd1
Greg Field <greg.field1992@gmail.com>
2024-02-11 22:13:49 +0000

    Refactor code to remove unnecessary server session retrieval

--- server/api/lists.get.ts
+++ server/api/lists.get.ts
@@ -1,9 +1,8 @@
-import { getToken, getServerSession } from '#auth'
+import { getToken } from '#auth'
 export default defineEventHandler(async (event) => {
   try {
     const token = await getToken({ event })
-    const session = await getServerSession(event)
-    const id = session.user._id ? session.user._id : token.sub
+    const id =  token.sub
 
     return await ListSchema.find({ userId: id })
   } catch (error) {
