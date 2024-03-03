d7e84b02348a3ba6d3edfa8763a52e7ebb782283
Greg Field <greg.field1992@gmail.com>
2024-03-03 17:58:10 +0000

    Add console log for hotjar installation

--- plugins/hotjar.ts
+++ plugins/hotjar.ts
@@ -3,6 +3,7 @@ import VueHotjar from "vue-hotjar-next";
 export default defineNuxtPlugin((nuxtApp) => {
     if (process.client) {
         const runtimeConfig = useRuntimeConfig()
+        console.log('hotjar installed ', Number(runtimeConfig.public.hotjarId))
         nuxtApp.vueApp.use(VueHotjar, {
             id: Number(runtimeConfig.public.hotjarId),
             isProduction: true, // <-- or simply true/false
