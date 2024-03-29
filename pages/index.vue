aa3542f1799d56596efff6b28deeffb512e3376b
Greg Field <greg.field1992@gmail.com>
2024-03-29 16:37:00 +0000

    Refactor index.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 const { data, status } = useAuth()
 const loggedIn = computed(() => status.value === 'authenticated')
 const listsStore = useListsStore()
@@ -20,10 +23,6 @@ definePageMeta({
     navigateUnauthenticatedTo: '/login',
   },
 })
-let userId = "";
-if (data.value && data.value?.user) {
-  userId = data.value?.user.id ? data.value?.user.id : data.value?.user.sub
-}
 
 const todaysTodos = computed(() => {
   return listsStore.todaysTodos.filter((todo) => todo.status !== 'Closed')
@@ -40,96 +39,97 @@ function selectTodo(todo: Todo) {
 
 </script>
 <template>
-      <v-col cols="12">
-        <v-tabs
-          v-model="tab"
-          align-tabs="center"
+  <v-col cols="12">
+    <v-tabs
+      v-model="tab"
+      align-tabs="center"
+    >
+      <v-tab>
+        todo
+      </v-tab>
+      <v-tab v-if="todaysClosedTodos.length">
+        done
+      </v-tab>
+    </v-tabs>
+    <v-window
+      v-model="tab"
+      class=""
+      style="min-height: 150px;"
+    >
+      <v-window-item value="todo">
+        <v-list v-if="todaysTodos.length">
+          <AppDialog
+            :open="dialog"
+            @close="dialog = false"
+          >
+            <TodoDetail />
+          </AppDialog>
+
+          <v-list-item
+            v-for="todo in todaysTodos"
+            :key="todo._id"
+            class="fill-height"
+            @click="selectTodo(todo)"
+          >
+
+            <template #prepend>
+              <ListStatus :todo="todo" />
+            </template>
+            <v-list-item-title class="ml-4">
+              {{ todo.name }}
+            </v-list-item-title>
+
+            <template #append>
+              <v-btn
+                icon="mdi-delete"
+                elevation="0"
+                variant="text"
+                size="small"
+                @click.stop="listsStore.deleteTodo(todo._id)"
+              />
+            </template>
+          </v-list-item>
+
+        </v-list>
+        <v-card
+          v-else
+          height="200"
+          variant="tonal"
         >
-          <v-tab>
-            todo
-          </v-tab>
-          <v-tab v-if="todaysClosedTodos.length">
-            done
-          </v-tab>
-        </v-tabs>
-        <v-window
-          v-model="tab"
-          class=""
-          style="min-height: 150px;"
-        >
-          <v-window-item value="todo">
-            <v-list v-if="todaysTodos.length">
-              <AppDialog
-                :open="dialog"
-                @close="dialog = false"
+          <v-card-item class="text-center">
+            Nothing todo today 🎉
+          </v-card-item>
+        </v-card>
+      </v-window-item>
+      <v-window-item value="done">
+        <v-list v-if="todaysClosedTodos.length">
+          <v-list-item
+            v-for="todo in todaysClosedTodos"
+            :key="todo._id"
+          >
+            <template #prepend>
+              <ListStatus :todo="todo" />
+            </template>
+            <v-list-item-title class="ml-4">
+              {{ todo.name }}
+            </v-list-item-title>
+
+            <template #append>
+              <v-btn
+                icon
+                elevation="0"
+                @click="listsStore.deleteTodo(todo._id)"
               >
-                <TodoDetail />
-              </AppDialog>
-
-              <v-list-item
-                v-for="todo in todaysTodos"
-                :key="todo._id"
-                class="fill-height"
-                @click="selectTodo(todo)"
-              >
-
-                <template #prepend>
-                  <ListStatus :todo="todo" />
-                </template>
-                <v-list-item-title class="ml-4">
-                  {{ todo.name }}
-                </v-list-item-title>
-
-                <template #append>
-                  <v-btn
-                    icon="mdi-delete"
-                    elevation="0"
-                    variant="text"
-                    size="small"
-                    @click.stop="listsStore.deleteTodo(todo._id)"
-                  />
-                </template>
-              </v-list-item>
-
-            </v-list>
-            <v-card
-              v-else
-              height="200"
-              class="d-flex align-center justify-center ma-2"
-            >
-              Nothing todo today 🎉
-            </v-card>
-          </v-window-item>
-          <v-window-item value="done">
-            <v-list v-if="todaysClosedTodos.length">
-              <v-list-item
-                v-for="todo in todaysClosedTodos"
-                :key="todo._id"
-              >
-
-                <template #prepend>
-                  <ListStatus :todo="todo" />
-                </template>
-                <v-list-item-title class="ml-4">
-                  {{ todo.name }}
-                </v-list-item-title>
-
-                <template #append>
-                  <v-btn
-                    icon
-                    elevation="0"
-                    @click="listsStore.deleteTodo(todo._id)"
-                  >
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </template>
-              </v-list-item>
-            </v-list>
-          </v-window-item>
-        </v-window>
-    </v-col>
-    <!-- TODO add reminders feature -->
-    <!-- <v-col >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+          </v-list-item>
+        </v-list>
+      </v-window-item>
+    </v-window>
+  </v-col>
+  <!-- TODO add reminders feature -->
+  <!-- <v-col >
       <v-card class="pa-4">
         <h2>reminders</h2>
       </v-card>
