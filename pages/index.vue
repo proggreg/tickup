6cbe1e9e11edf7d677abf0dde64b7d5caed77fa0
Greg Field <greg.field1992@gmail.com>
2024-03-29 21:56:29 +0000

    Refactor index.vue script setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,4 @@
-<script
-  setup
-  lang="ts"
->
-const { data, status } = useAuth()
+<script setup lang="ts">
 const loggedIn = computed(() => status.value === 'authenticated')
 const listsStore = useListsStore()
 const tab = ref('todo')
