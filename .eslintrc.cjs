b402cda31c0639847ecd81b758995ea1533011fc
Greg Field <greg.field1992@gmail.com>
2024-03-30 19:22:12 +0000

    Add new ESLint rule to disable first-attribute-linebreak

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'vue/valid-v-slot': 'off',
     'vue/max-attributes-per-line': 'off',
     'vue/html-indent': 'off',
-    'vue/multiline-html-element-content-newline': 'off'
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/first-attribute-linebreak': 'off',
   }
 }
