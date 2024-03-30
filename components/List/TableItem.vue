14d8ed5bd36b18d5d5eeb32bc5f2b0aaa0892051
Greg Field <greg.field1992@gmail.com>
2024-03-30 22:20:06 +0000

    go to todo page

--- components/List/TableItem.vue
+++ components/List/TableItem.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 const { groupItem, columns } = defineProps(['groupItem', 'columns'])
 const store = useListsStore()
-const dialog = ref(false)
 const { xs } = useDisplay()
 
 function showModal(todo: any) {
   store.setCurrentTodo(todo.raw);
-  dialog.value = true
+  navigateTo(`/todo/${todo.raw._id}`);
 }
 function formatDate(date: Date) {
   if (!date) {
@@ -22,42 +21,22 @@ function deleteItem(todo: Todo) {
 }
 </script>
 <template>
-  <tr
-  v-for="item in groupItem.items"
-  :key="item.key"
-  @click="showModal(item)"
->
-  <td>
-    <ListStatus :todo="item.raw" />
-  </td>
-  <template
-    v-for="column in columns"
-    :key="column.key"
-  >
-    <template v-if="column.key !== 'data-table-group'">
-      <td v-if="column.key === 'name' || (column.key === 'desc' && !xs)">
-        {{ item.columns[column.key] }}
-      </td>
-      <td v-else-if="column.key === 'dueDate' && !xs">
-        {{ formatDate(item.columns[column.key]) }}
-      </td>
-      <td v-else-if="column.key === 'actions' && !xs">
-        <v-btn
-          icon="mdi-delete"
-          variant="text"
-          rounded="lg"
-          elevation="0"
-          small
-          @click.stop="deleteItem(item)"
-        />
-      </td>
+  <tr v-for="item in groupItem.items" :key="item.key" @click="showModal(item)">
+    <td>
+      <ListStatus :todo="item.raw" />
+    </td>
+    <template v-for="column in columns" :key="column.key">
+      <template v-if="column.key !== 'data-table-group'">
+        <td v-if="column.key === 'name' || (column.key === 'desc' && !xs)">
+          {{ item.columns[column.key] }}
+        </td>
+        <td v-else-if="column.key === 'dueDate' && !xs">
+          {{ formatDate(item.columns[column.key]) }}
+        </td>
+        <td v-else-if="column.key === 'actions' && !xs">
+          <v-btn icon="mdi-delete" variant="text" rounded="lg" elevation="0" small @click.stop="deleteItem(item)" />
+        </td>
+      </template>
     </template>
-  </template>
-</tr>
-<AppDialog
-  :open="dialog"
-  @close="dialog = false"
->
-  <TodoDetail />
-</AppDialog>
+  </tr>
 </template>
