b1c0260a8fd737665ed89550cb4150661fd9f5ba
Greg Field <greg.field1992@gmail.com>
2024-04-21 14:14:05 +0100

    refactor and improve layout

--- components/App/Dialog.vue
+++ components/App/Dialog.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const { smAndDown } = useDisplay()
-const dialogProps = defineProps<{
+const { title, open } = defineProps<{
   title?: string,
   open: boolean
 }>()
@@ -9,22 +9,24 @@ const emit = defineEmits(['close'])
 </script>
 
 <template>
-  <v-dialog
-  max-width="500" :model-value="dialogProps.open" :fullscreen="smAndDown" 
-  transition="dialog-bottom-transition" location="top"
-  @update:model-value="emit('close')"
->
+  <v-dialog max-width="500" :model-value="open" :fullscreen="smAndDown" transition="dialog-bottom-transition"
+    location="top" @update:model-value="emit('close')">
     <template #activator>
       <slot name="open" />
     </template>
 
     <v-card class="pa-12">
+      <v-card-title class="font-weight-bold">
+        {{ title }}
+      </v-card-title>
       <slot />
       <v-card-actions>
+        <v-spacer />
         <v-btn @click="emit('close')">
           Close
         </v-btn>
         <slot name="buttons" />
+
       </v-card-actions>
     </v-card>
   </v-dialog>
