47e16fe36b60239cdc8ebd96277a8fb46cfd9f3b
Greg Field <greg.field1992@gmail.com>
2024-05-06 18:45:06 +0100

    Refactor NavItems.vue component to improve code readability and remove unnecessary lines

--- components/App/NavItems.vue
+++ components/App/NavItems.vue
@@ -19,21 +19,15 @@ function renameList(list: List) {
 
 </script>
 <template>
-  <v-list>
-    <v-list-item v-if="!lists || !lists.length">
-      <v-list-item-title>No lists yet</v-list-item-title>
-    </v-list-item>
-    <v-list-item v-for="list in lists" v-else :key="list._id" color="accent" fluid placeholder="My List"
-      @click.passive="selectList(list)">
-      <v-text-field v-if="editListName === list._id" v-model="list.name" variant="plain" @keyup.enter="renameList(list)"
-        @blur="renameList(list)" />
-      <v-list-item-title v-else>
-        {{ list.name }}
-      </v-list-item-title>
+  <v-list-item v-for="list in lists" :key="list._id" link @click.passive="selectList(list)">
+    <v-text-field v-if="editListName === list._id" v-model="list.name" variant="plain" @keyup.enter="renameList(list)"
+      @blur="renameList(list)" />
+    <v-list-item-title v-else>
+      {{ list.name }}
+    </v-list-item-title>
 
-      <template #append>
-        <ListOptions v-if="list._id" size="small" :list-id="list._id" @rename="editListName = list._id" />
-      </template>
-    </v-list-item>
-  </v-list>
+    <template #append>
+      <ListOptions v-if="list._id" size="small" :list-id="list._id" @rename="editListName = list._id" />
+    </template>
+  </v-list-item>
 </template>
