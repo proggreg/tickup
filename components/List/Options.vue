3085fe1057176e4ed490f1b620923b9ee5b4e297
Greg Field <greg.field1992@gmail.com>
2024-05-12 13:26:21 +0100

    improve list options UI

--- components/List/Options.vue
+++ components/List/Options.vue
@@ -18,9 +18,12 @@ function renameList() {
 const options = reactive([{
   name: 'Rename',
   handler: renameList,
+  icon: 'mdi-pencil',
 }, {
   name: 'Delete',
   handler: deleteList,
+  icon: 'mdi-delete',
+  destructive: true,
 }])
 </script>
 
@@ -30,7 +33,11 @@ const options = reactive([{
       <v-btn v-bind="props" icon="mdi-dots-horizontal" variant="text" :size="size" />
     </template>
     <v-list class="px-2">
-      <v-list-item v-for="(option, index) in options" :key="index" :value="option.name" @click="option.handler">
+      <v-list-item v-for="(option, index) in options" 
+      :key="index" :value="option.name"
+      :append-icon="option.icon" 
+      :class="option.destructive ? 'text-red' : ''"
+      @click="option.handler">
         <v-list-item-title class="text-body-2">
           {{ option.name }}
         </v-list-item-title>
