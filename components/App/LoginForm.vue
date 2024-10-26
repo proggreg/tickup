5835f4b10b4bacb4b58e08f77d9b71b596fb061a
Greg Field <greg.field1992@gmail.com>
2024-10-26 17:31:31 +0100

    make font size constistant

--- components/App/LoginForm.vue
+++ components/App/LoginForm.vue
@@ -12,7 +12,7 @@ const userNameRules = [
   },
   () => {
     return true
-  }
+  },
 ]
 const passwordRules = [
   (value: string) => {
@@ -33,60 +33,62 @@ const loginUser = async () => {
   if (valid) {
     await signIn('credentials', { username: username.value, password: password.value })
   }
-
 }
 </script>
-<template>
-    <v-form
-      ref="loginForm"
-      validate-on="submit input"
-      @submit.prevent="loginUser"
-    >
-      <v-text-field
-        v-model="username"
-        label="Username"
-        :hide-details="false"
-        :rules="userNameRules"
-        required
-        class="error"
-      />
-    
-      <v-text-field
-        v-model="password"
-        label="Password"
-        :hide-details="false"
-        :rules="passwordRules"
-        type="password"
-        required
-      />
-          <v-btn
-          class="mb-4"
-            color="primary"
-            block
-            type="submit"
-          >
-            Login
-          </v-btn>
-          <v-btn
-            color="primary"
-            append-icon="mdi-github"
-            class="mb-4"
-            block
-            @click="signIn(`github`)"
-          >
-            Github Sign In
-          </v-btn>
-          <span>Don't have an account </span>
-          <NuxtLink
-            color="secondary"
-            to="/register"
-          >Register</NuxtLink>
-      <v-snackbar
-        v-model="correctCredentials"
-        color="danger"
-      >
-        Incorrect Credentials
-      </v-snackbar>
-    </v-form>
 
+<template>
+  <v-form
+    ref="loginForm"
+    validate-on="submit input"
+    @submit.prevent="loginUser"
+  >
+    <v-text-field
+      v-model="username"
+      label="Username"
+      :hide-details="false"
+      :rules="userNameRules"
+      required
+      class="error"
+    />
+
+    <v-text-field
+      v-model="password"
+      label="Password"
+      :hide-details="false"
+      :rules="passwordRules"
+      type="password"
+      required
+    />
+    <v-btn
+      class="mb-4"
+      color="primary"
+      block
+      type="submit"
+    >
+      Login
+    </v-btn>
+    <v-btn
+      color="primary"
+      append-icon="mdi-github"
+      class="mb-4"
+      block
+      @click="signIn(`github`)"
+    >
+      Github Sign In
+    </v-btn>
+    <div class="text-caption text-center">
+      <span>Don't have an account </span>
+      <NuxtLink
+        color="secondary"
+        to="/register"
+      >Register
+      </NuxtLink>
+    </div>
+    <v-snackbar
+      v-model="correctCredentials"
+      color="danger"
+    >
+      Incorrect Credentials
+    </v-snackbar>
+  </v-form>
 </template>
