fb313980bf970f28702caed6ece6c966bb052bf9
Greg Field <greg.field1992@gmail.com>
2024-11-27 22:04:42 +0000

    do more filtering

--- components/App/GithubButton.vue
+++ components/App/GithubButton.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 const { todoName } = defineProps(['todoName'])
-const githubBranchName = computed(() => `git checkout -b ${todoName.replace(/ /g, '-').toLowerCase()}`)
+const filteredTodoName = computed(() => todoName.replace(/[\u{1F300}-\u{1F9FF}]|[\u{2700}-\u{27BF}]|[\u{1F000}-\u{1F02F}]|[\u{1F0A0}-\u{1F0FF}]|[\u{1F100}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{1F900}-\u{1F9FF}]|[\u{2600}-\u{26FF}]|[\u{2B00}-\u{2BFF}]/gu, '').trim())
+const githubBranchName = computed(() => {
+    let branchName = filteredTodoName.value.replace(/[!@#$%^&*(),.?":{}|<>]/g, '').replace(/ /g, '-').toLowerCase()
+    if (branchName.charAt(branchName.length - 1) === '-') {
+        branchName = branchName.slice(0, -1)
+    }
+    return `git checkout -b ${branchName}`
+})
+
 const open = ref(false)
 const copyToClipBoard = () => {
     if (githubBranchName.value) {
