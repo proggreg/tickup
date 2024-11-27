2ec9ecbef5043dd55293f03d0f6733a2f34de41b
Greg Field <greg.field1992@gmail.com>
2024-11-27 22:27:01 +0000

    chore: Add account page and update mobile navigation

--- components/App/MobileNav.vue
+++ components/App/MobileNav.vue
@@ -5,25 +5,30 @@ function navClick(value: string) {
 }
 </script>
 <template>
-<v-bottom-navigation v-if="smAndDown" style="position: fixed;" >
-  <v-btn to="/" value="home">
-    <v-icon>mdi-home</v-icon>
-    <span>Home</span>
-  </v-btn>
+  <v-bottom-navigation v-if="smAndDown" style="position: fixed;">
+    <v-btn to="/" value="home">
+      <v-icon>mdi-home</v-icon>
+      <span>Home</span>
+    </v-btn>
 
-  <v-btn to="/lists" value="lists">
-    <v-icon>mdi-format-list-bulleted</v-icon>
-    <span>Lists</span>
-  </v-btn>
+    <v-btn to="/lists" value="lists">
+      <v-icon>mdi-format-list-bulleted</v-icon>
+      <span>Lists</span>
+    </v-btn>
 
-  <v-btn to="/search" value="search">
-    <v-icon>mdi-magnify</v-icon>
-    <span>Search</span>
-  </v-btn>
+    <v-btn to="/search" value="search">
+      <v-icon>mdi-magnify</v-icon>
+      <span>Search</span>
+    </v-btn>
 
-  <v-btn to="/settings" value="settings">
-    <v-icon>mdi-cog</v-icon>
-    <span>Settings</span>
-  </v-btn>
-</v-bottom-navigation>
+    <v-btn to="/settings" value="settings">
+      <v-icon>mdi-cog</v-icon>
+      <span>Settings</span>
+    </v-btn>
+
+    <v-btn to="/account" value="settings">
+      <v-icon>mdi-account-circle</v-icon>
+      <span>Account</span>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
---
+++ pages/account.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+    title: 'Account',
+    description: 'Account page',
+    layout: 'settings'
+})
+
+const { signOut, data } = useAuth()
+</script>
+<template>
+    <div class="d-flex flex-column">
+        My Account
+        <v-btn class="my-auto" @click="signOut">Logout</v-btn>
+    </div>
+</template>
