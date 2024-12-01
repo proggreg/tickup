8bdcd623dfff9607b28136fb661b1677d53b7838
Greg Field <greg.field1992@gmail.com>
2024-12-01 22:57:59 +0000

    Refactor imports in nuxt.config.ts and remove unused code

--- server/api/todo/[_id].get.ts
+++ server/api/todo/[_id].get.ts
@@ -1,10 +1,10 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (event): Promise<Todo | null> => {
   try {
     if (!event.context.params || !event.context.params._id) {
       throw new Error('no id')
     }
     return await TodoSchema.findById(event.context.params._id)
   } catch (e) {
-    return e
+    throw e
   }
 })
