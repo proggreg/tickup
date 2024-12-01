c1885b28d073582f4092ed3f0400e88985992516
Greg Field <greg.field1992@gmail.com>
2024-12-01 11:19:44 +0000

    Refactor webhook API to handle push events and update corresponding todos

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -22,7 +22,29 @@ export default defineEventHandler(async (event) => {
     
     const githubEvent = event.headers.get('X-GitHub-Event')
 
-    if (githubEvent === 'delete') {
+    if (githubEvent === 'push') {
+      // Handle push event
+      console.log('Received push event:', body)
+      console.log('payload:', body.payload)
+
+      try {
+        const payload = JSON.parse(body.payload)
+
+        console.log('payload: ref', payload.ref)
+        
+        const response = await TodoSchema.findOneAndUpdate({
+        githubBranchName: payload.ref
+      }, {status: 'In Progress'}, { new: true })
+      console.log('Updated todo:', response)
+      } catch (error) {
+        console.error('Error:', error)
+      }
+
+      return {
+        status: 'success',
+        message: 'Webhook received'
+      }
+    } else if (githubEvent === 'delete') {
         // Handle delete event
         console.log('Received delete event:', body)
         console.log('payload:', body.payload)
