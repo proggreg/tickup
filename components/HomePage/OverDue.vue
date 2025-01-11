eb4cdac0c6c5ff38e4834feebb08b91db780459a
Greg Field <greg.field1992@gmail.com>
2025-01-11 19:13:01 +0000

    Overdue-tasks-on-homepage (#42)

--- components/HomePage/OverDue.vue
+++ components/HomePage/OverDue.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const listsStore = useListsStore()
 const { isMobile } = useDevice()
-const showClosed = ref(true)
+const opened = ref(['Open'])
 
 function selectTodo(todo: Todo) {
   listsStore.setCurrentTodo(todo)
@@ -35,55 +35,70 @@ const closedTodos = computed(() => {
   <v-card
     v-if="listsStore.overdueTodos && listsStore.overdueTodos.length"
   >
-    <v-list variant="plain">
-      <v-list-subheader>Open</v-list-subheader>
-      <v-list-item
-        v-for="todo in openTodos"
-        :key="todo._id"
-        slim
-        @click="selectTodo(todo)"
-      >
-        <template #prepend>
-          <v-checkbox @click.stop="setClosed(todo)" />
+    <v-list :opened="opened" variant="plain">
+      <v-list-group value="Open">
+        <template #activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi mdi-border-all-variant"
+            title="Open"
+          />
         </template>
-        <v-list-item-title class="text-h6">
-          {{ todo.name }}
-        </v-list-item-title>
 
-        <v-list-item-subtitle v-if="todo.dueDate">
-          {{ formatDate(todo.dueDate) }}
-        </v-list-item-subtitle>
+        <v-list-item
+          v-for="todo in openTodos"
+          :key="todo._id"
+          slim
+          @click="selectTodo(todo)"
+        >
+          <template #prepend>
+            <v-checkbox @click.stop="setClosed(todo)" />
+          </template>
+          <v-list-item-title class="text-h6">
+            {{ todo.name }}
+          </v-list-item-title>
 
-        <template #append>
-          <AppDeleteButton :todo="todo" />
+          <v-list-item-subtitle v-if="todo.dueDate">
+            {{ formatDate(todo.dueDate) }}
+          </v-list-item-subtitle>
+
+          <template #append>
+            <AppDeleteButton :todo="todo" />
+          </template>
+        </v-list-item>
+      </v-list-group>
+
+      <v-list-group value="Closed">
+        <template #activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi mdi-check-all"
+            title="Closed"
+          />
         </template>
-      </v-list-item>
-    </v-list>
-    <v-list variant="plain">
-      <v-list-subheader @click="showClosed = !showClosed">Closed</v-list-subheader>
 
-      <v-list-item
-        v-for="todo in closedTodos"
+        <v-list-item
+          v-for="todo in closedTodos"
+          :key="todo._id"
+          slim
+          @click="selectTodo(todo)"
+        >
+          <template #prepend>
+            <v-checkbox :model-value="true" @click.stop="setOpen(todo)" />
+          </template>
+          <v-list-item-title class="text-h6">
+            {{ todo.name }}
+          </v-list-item-title>
 
-        :key="todo._id"
-        slim
-        @click="selectTodo(todo)"
-      >
-        <template #prepend>
-          <v-checkbox :model-value="true" @click.stop="setOpen(todo)" />
-        </template>
-        <v-list-item-title class="text-h6">
-          {{ todo.name }}
-        </v-list-item-title>
+          <v-list-item-subtitle v-if="todo.dueDate">
+            {{ formatDate(todo.dueDate) }}
+          </v-list-item-subtitle>
 
-        <v-list-item-subtitle v-if="todo.dueDate">
-          {{ formatDate(todo.dueDate) }}
-        </v-list-item-subtitle>
-
-        <template #append>
-          <AppDeleteButton :todo="todo" />
-        </template>
-      </v-list-item>
+          <template #append>
+            <AppDeleteButton :todo="todo" />
+          </template>
+        </v-list-item>
+      </v-list-group>
     </v-list>
   </v-card>
   <v-card
