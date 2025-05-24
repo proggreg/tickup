5747a3106091a386849cbc165164a16335ccd89b
Greg Field <greg.field1992@gmail.com>
2025-05-24 08:47:32 +0100

    Add settings components: Created List.vue and Profile.vue for settings management, and implemented Status.vue for handling status updates with options to rename and delete. Removed old settings page and integrated new components into the settings index page.

---
+++ pages/settings/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'settings',
+})
+const store = useSettingsStore()
+await useAsyncData(() => store.getUserSettings().then(() => true))
+
+</script>
+
+<template>
+  <v-container>
+  <v-row>
+    <v-col cols="12">
+      <h2 class="text-center">Settings</h2>
+    </v-col>
+    <v-col cols="4">  
+     <SettingsStatus />
+    </v-col>
+  </v-row>
+  </v-container>
+</template>
