9ca097ce6c52f2a4dca126e06f0124b73a4c1cec
Greg Field <greg.field1992@gmail.com>
2025-08-02 20:39:06 +0100

    clean up

--- app.vue
+++ app.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-const listsStore = useListsStore()
-const settingsStore = useSettingsStore()
-const { data, status } = useAuth()
-const { isMobile } = useDevice()
-const route = useRoute()
-const config = useRuntimeConfig()
-const event = useRequestEvent()
-const dialog = useDialog()
-const { $pwa } = useNuxtApp()
+  const listsStore = useListsStore()
+  const settingsStore = useSettingsStore()
+  const { data, status } = useAuth()
+  const config = useRuntimeConfig()
+  const event = useRequestEvent()
+  
+  useShortcutKeys()
 
 if (status.value === 'authenticated') {
   await useAsyncData(() => settingsStore.getUserSettings().then(() => true))
@@ -29,65 +27,20 @@ if (data?.value?.user?.sub) {
   await useAsyncData('lists', () => listsStore.getLists(data?.value?.user?.sub ? data?.value?.user?.sub : '').then(() => true))
 }
 
-onBeforeMount(() => {
-  const userId = data?.value?.user?.sub
-  console.log('userId', userId)
-  if (userId) {
-    listsStore.getLists(userId)
-  }
-
-  if (route.params.id) {
-    const { data: currentList } = useFetch<List>(`/api/list/${route.params.id}`)
-
-    if (currentList.value) {
-      listsStore.setCurrentList(currentList.value)
-    }
-  }
-})
-
-onMounted(() => {
-  console.log('App mounted pwa ', $pwa)
-  if ($pwa.offlineReady) {
-    console.log('App ready to work offline')
-  }
-})
-const layoutName = computed(() => {
-  if (route.name === 'login' || route.name === 'register') {
-    return 'login-register'
-  }
-  if (isMobile) {
-    return 'mobile'
-  }
-  if (route.name === 'todo-id') {
-    return 'todo'
-  }
-
-  return 'default'
-})
 </script>
 
 <template>
   <div>
-    <NuxtPwaManifest />
-    <AppKeyCommands />
-    <!-- <div v-show="$pwa.needRefresh">
-      <span>
-        New content available, click on reload button to update.
-      </span>
-
-      <button @click="$pwa.updateServiceWorker()">
-        Reload
-      </button>
-    </div> -->
+    <NuxtPwaManifest />   
     <AppDialog page="todo" title="New Todo">
-      <TodoNew @save-todo="dialog.open = false" />
+      <TodoNew />
       <template #buttons>
-        <v-btn color="primary" :disabled="listsStore.newTodo.name === ''" @click.stop="dialog.open = false">
+        <v-btn color="primary" :disabled="listsStore.newTodo.name === ''">
           Save
         </v-btn>
-      </template>
+      </template> 
     </AppDialog>
-    <NuxtLayout :name="layoutName">
+    <NuxtLayout :name="useAppLayout()">
       <NuxtPage />
     </NuxtLayout>
   </div>
