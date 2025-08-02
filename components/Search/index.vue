9df24614c47542991a16511fa46b44b385a73ea8
Greg Field <greg.field1992@gmail.com>
2025-08-02 20:51:00 +0100

    Update Search component to enhance user interaction by modifying the text field to open the dialog on click

--- components/Search/index.vue
+++ components/Search/index.vue
@@ -31,7 +31,7 @@ watch(() => searchStore.searchQuery, () => {
 <template>
   <v-dialog class="ma-6" width="500" min-height="300" max-height="100%" height="100%" :model-value="open" @after-leave="open = false">
     <template #activator="{ props }">
-      <v-text-field  />
+      <v-text-field @click="open = true" placeholder="ctrl + k" class="mx-6" append-inner-icon="mdi-magnify" v-on="props" />
     </template>
 
     <template #default="{ isActive }">
