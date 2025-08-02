17b28f4f6f968a7a70f8aae2bdd7e5c5ad80ba0c
Greg Field <greg.field1992@gmail.com>
2025-08-02 20:43:45 +0100

    Refactor Create Todo tests to streamline functionality and improve readability

--- e2e/todos/create-test.spec.ts
+++ e2e/todos/create-test.spec.ts
@@ -1,35 +1,34 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, } from '@playwright/test'
 
 
 test.describe('Create Todo', () => {
-  test('create todo in a list', async ({ page }) => {
-    const todoListName = 'Todo List'
-    await page.goto('/')
+  test('in a list', async ({ page }) => {
+    await page.goto('/list/688e2e5d0ecb2728667ebe40')
+    await page.waitForLoadState('networkidle')    
+    const newTodoInput = await page.getByPlaceholder('Add todo to Todo List')
+    const testId = new Date()
+    const todoName = `New Todo test ${testId}`
+    await newTodoInput.fill(todoName)
+    await newTodoInput.press('Enter')
     await page.waitForLoadState('networkidle')
-    const navItems = await page.locator('.v-list-item-title').all()
-    const listNames = await Promise.all(navItems.map(navItem => navItem.textContent()))
-    
-    if (!listNames.includes(todoListName)) {
-      await page.getByTestId('new-list-button').click()
-      await page.getByPlaceholder('New List').fill(todoListName)
-      await page.keyboard.press('Enter')
-      
-    } else {
-
-      // TODO don't use listid for nav
-      await page.goto('/list/688e2e5d0ecb2728667ebe40')
-      const newTodoInput = await page.getByPlaceholder('Add todo to Todo List')
-      const testId = new Date()
-      const todoName = `New Todo test ${testId}`
-      await newTodoInput.fill(todoName)
-      await newTodoInput.press('Enter')
-      await page.waitForLoadState('networkidle')
-      await page.screenshot({ path: 'newtodo.png'})
-
-      const listItemTitlesEls = await page.locator('.v-list-item-title').all()
-      const listItemTitles = await Promise.all(listItemTitlesEls.map(navItem => navItem.textContent()))
+    const listItemTitlesEls = await page.getByTestId('todo-title').all()
+    const listItemTitles = await Promise.all(listItemTitlesEls.map(navItem => navItem.textContent()))
   
-      expect(listItemTitles.includes(todoName)).toBeTruthy()
-    }
+    expect(listItemTitles.includes(todoName)).toBeTruthy()
+  })
+
+  test('in a table', async ({ page }) => {
+    await page.goto('/list/688e660a9f3371ad3648103a')
+    await page.waitForLoadState('networkidle')    
+    const newTodoInput = await page.getByPlaceholder('Add todo to Table Todos')
+    const testId = new Date()
+    const todoName = `New Todo test ${testId}`
+    await newTodoInput.fill(todoName)
+    await newTodoInput.press('Enter')
+    await page.waitForLoadState('networkidle')
+    const listItemTitlesEls = await page.getByTestId('todo-title').all()
+    const listItemTitles = await Promise.all(listItemTitlesEls.map(navItem => navItem.textContent()))
+  
+    expect(listItemTitles.includes(todoName)).toBeTruthy()
   })
 })
