aa05991769c2c4023412c7042a0d5add6ce7f617
Greg Field <greg.field1992@gmail.com>
2025-11-01 13:56:09 +0000

    protect homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'default'
+  layout: 'default',
+  auth: { authenticatedOnly: true, navigateUnauthenticatedTo: '/login' }
 })
 useHead({ title: 'TickUp:Home' })
 
