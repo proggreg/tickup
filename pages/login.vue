9e86fc8d31ccbaa0d5b1ae3ee7fa60664e6df563
Greg Field <greg.field1992@gmail.com>
2025-11-02 11:50:01 +0000

    try password login

--- pages/login.vue
+++ pages/login.vue
@@ -4,11 +4,20 @@
   })
   const config = useRuntimeConfig()
   const supabase = useSupabaseClient()
+  const user = useSupabaseUser()
+
   const email = ref('')
+  const password = ref('')
   const BASE_URL = config.public.VERCEL_URL ?? 'localhost:3000' 
   console.log('Base url', BASE_URL)
   console.log('public', config.public)
   console.log('redirect to', `https://${BASE_URL}/confirm`)
+
+  watchEffect(() => {
+    if (user.value) {
+      return navigateTo('/')
+    }
+  })
 
   const signInWithOtp = async () => {
     const { error } = await supabase.auth.signInWithOtp({
@@ -19,12 +28,30 @@
     })
     if (error) console.log(error)
   }
+
+  function signInWithPassword() {
+    supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value
+    })
+  }
+
+  function signUpWithPassword() {
+    supabase.auth.signUp({
+      email: email.value,
+      password: password.value
+    })
+  }
 </script>
 <template>
   <div>
-    <v-text-field v-model="email" />
-    <v-btn  @click="signInWithOtp">
+    <v-text-field v-model="email" placeholder="email" />
+    <v-text-field v-model="password" type="password" placeholder="password" />
+    <v-btn  @click="signInWithPassword">
       Sign In with E-Mail
+    </v-btn>
+    <v-btn  @click="signUpWithPassword">
+      Sign Up with E-Mail
     </v-btn>
 
   </div>
