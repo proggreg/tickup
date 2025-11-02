de51702537de5198e244796a6f97e473bfb1e632
Greg Field <greg.field1992@gmail.com>
2025-11-02 10:39:47 +0000

    remove old auth middleware

--- server/middleware/auth.ts
+++
@@ -1,4 +0,0 @@
-export default defineEventHandler(async (_event) => {
-  // Skip authentication for AWS routes for now - will be handled by client-side middleware
-  // This avoids the cookie parsing issues we're experiencing with server-side Supabase
-})
