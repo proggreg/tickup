1e76f28dd0957c5749b8822da84d5b190a79372f
Greg Field <greg.field1992@gmail.com>
2026-01-04 10:07:50 +0000

    remove unused dragging state and related computed properties from Board component

--- app/components/Board/index.vue
+++ app/components/Board/index.vue
@@ -1,33 +1,7 @@
 <script setup lang="ts">
 const statusStore = useSettingsStore();
-// const { $device } = useNuxtApp();
-const dragging = ref(false);
-const listStore = useListsStore();
 
 const boardRef = ref<HTMLElement | null>(null);
-
-const cardHeight = computed(() => {
-    const boardElement = boardRef.value;
-    const parentElement = boardElement?.parentElement;
-    const parentHeight = parentElement ? parentElement.clientHeight : window.innerHeight;
-    return (parentHeight - 30) + 'px';
-});
-
-const cardWidth = computed(() => {
-    const boardElement = boardRef.value;
-    const parentElement = boardElement?.parentElement;
-    const parentWidth = parentElement ? parentElement.clientWidth : window.innerWidth;
-    return (parentWidth / 10) + 'px';
-});
-
-watch(dragging, (isDragging: boolean) => {
-    if (isDragging) {
-        document.body.style.cursor = 'grabbing';
-    }
-    else {
-        document.body.style.cursor = 'auto';
-    }
-});
 </script>
 
 <template>
@@ -42,8 +16,6 @@ watch(dragging, (isDragging: boolean) => {
         >
             <BoardColumn
                 :status="status"
-                :card-height="cardHeight"
-                :card-width="cardWidth"
             />
         </v-slide-group-item>
     </v-slide-group>
