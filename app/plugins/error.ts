7f3e2e475aae944a35290e11176064d7d7e938a3
Greg Field <greg.field1992@gmail.com>
2026-03-01 18:43:33 +0000

    Refactor error handling in Nuxt plugin to improve error reporting
    - Removed the custom error handler and updated the error notification to stringify the error object for better context in Bugsnag.

--- app/plugins/error.ts
+++ app/plugins/error.ts
@@ -1,12 +1,6 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
-    // handle error, e.g. report to a service
-
-        useBugsnag().notify(new Error('Some Error'));
-    };
     nuxtApp.hook('vue:error', (err) => {
         console.log('nuxt error', err);
-        debugger;
-        useBugsnag().notify(new Error(err));
+        useBugsnag().notify(new Error(JSON.stringify(err)));
     });
 });
