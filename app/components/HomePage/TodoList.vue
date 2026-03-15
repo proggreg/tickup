68b8ea76791d69e38495b33fb147b56057fc057c
Greg Field <greg.field1992@gmail.com>
2026-03-15 09:20:57 +0000

    Refactor todo status handling across components
    - Introduced a new composable `useTodoStatus` to centralize the logic for determining if a todo is closed.
    - Updated `Today.vue`, `TodayClosed.vue`, and `TodoList.vue` to utilize the new `isTodoClosed` function for filtering todos based on their status.
    - Adjusted the email used in the global setup for e2e tests.
    - Added a new e2e test for verifying the loading of legacy done todos in the Done tab.

--- app/components/HomePage/TodoList.vue
+++ app/components/HomePage/TodoList.vue
@@ -14,14 +14,15 @@ const props = withDefaults(defineProps<Props>(), {
 
 // const { isMobile } = useDevice()
 const { selectTodo, setClosed, setOpen, formatDate } = useTodoActions();
+const { isTodoClosed } = useTodoStatus();
 const opened = ref(['Open']);
 
 const openTodos = computed(() => {
-    return props.todos.filter((todo: Todo) => todo.status !== 'Closed');
+    return props.todos.filter((todo: Todo) => !isTodoClosed(todo.status));
 });
 
 const closedTodos = computed(() => {
-    return props.todos.filter((todo: Todo) => todo.status === 'Closed');
+    return props.todos.filter((todo: Todo) => isTodoClosed(todo.status));
 });
 
 const handleSetClosed = (todo: Todo, event?: any) => {
@@ -143,7 +144,7 @@ const handleSetOpenSimple = (todo: Todo) => {
             >
                 <template #prepend>
                     <v-checkbox
-                        v-if="todo.status !== 'Closed'"
+                        v-if="!isTodoClosed(todo.status)"
                         @click.stop="handleSetClosedSimple(todo)"
                     />
                     <v-checkbox
