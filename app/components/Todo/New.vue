9b8774de1e85457dd2e45e8bb32d81ba363a24c2
Greg Field <greg.field1992@gmail.com>
2026-03-19 07:02:52 +0000

    New-todo-dialog-doesn't-close-after-pressing-enter (#101)

--- app/components/Todo/New.vue
+++ app/components/Todo/New.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 const listsStore = useListsStore();
+const dialog = useDialog();
 
 function addTodo() {
     listsStore.addTodo();
+    dialog.value.open = false;
 }
 </script>
 
