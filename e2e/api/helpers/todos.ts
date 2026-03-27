fe6457a4c91a3728a456aea64bbad940622140da
Greg Field <greg.field1992@gmail.com>
2026-03-27 08:21:55 +0000

    add return type

--- e2e/api/helpers/todos.ts
+++ e2e/api/helpers/todos.ts
@@ -3,10 +3,10 @@ import type { Todo } from '../../../index';
 
 export async function createTodo(request, todo: {
     name: string;
-    dueDate: Date;
+    dueDate?: Date;
     listId?: string;
     parentId?: string;
-}) {
+}): Promise<Todo> {
     const payload: Todo = { ...createNewTodoState(), ...todo };
 
     const response = await request.post('/api/todo', {
