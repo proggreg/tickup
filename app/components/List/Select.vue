c698d87e187b845311864015811c70bac1ff3c84
Greg Field <greg.field1992@gmail.com>
2026-04-04 19:12:38 +0100

    add list selection component

---
+++ app/components/List/Select.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const listsStore = useListsStore();
+</script>
+
+<template>
+    <v-select
+        v-model="listsStore.currentList.id"
+        :items="listsStore.lists"
+        item-title="name"
+        item-value="id"
+        min-width="75"
+        data-testid="list-select"
+    />
+</template>
