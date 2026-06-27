46c711280d9eed54085513c341538d71ef5ca85d
Greg Field <greg.field1992@gmail.com>
2026-06-27 18:37:37 +0100

    fix: ensure minimum width for status columns in board layout

--- app/components/Board/index.vue
+++ app/components/Board/index.vue
@@ -5,7 +5,7 @@ const statusStore = useSettingsStore();
 <template>
   <div class="overflow-x-auto overflow-y-hidden pa-1 pb-6" style="height: calc(100vh - 180px)">
     <v-row no-gutters class="flex-nowrap align-start" style="min-width: max-content; height: 100%">
-      <v-col v-for="status in statusStore.statuses" :key="status.name" class="pr-6">
+      <v-col v-for="status in statusStore.statuses" :key="status.name" class="pr-6" min-width="300">
         <BoardColumn :status="status" />
       </v-col>
     </v-row>
