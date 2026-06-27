9639d260762f32013175fd74f2119158e4eca779
Greg Field <greg.field1992@gmail.com>
2026-06-27 15:15:40 +0100

    formatting

--- app/layouts/todo.vue
+++ app/layouts/todo.vue
@@ -3,26 +3,18 @@ const colorMode = useColorMode();
 </script>
 
 <template>
-    <ColorScheme>
-        <v-theme-provider
-            with-background
-            :theme="colorMode.preference"
-        >
-            <v-app>
-                <v-layout>
-                    <app-nav />
-                    <v-main>
-                        <v-container
-                            style="height: 100%"
-                            fluid
-                        >
-                            <v-row>
-                                <NuxtPage />
-                            </v-row>
-                        </v-container>
-                    </v-main>
-                </v-layout>
-            </v-app>
-        </v-theme-provider>
-    </ColorScheme>
+  <ColorScheme>
+    <v-theme-provider with-background :theme="colorMode.preference">
+      <v-app>
+        <v-layout>
+          <app-nav />
+          <v-main>
+            <v-container class="full-height px-8 py-4" fluid>
+              <NuxtPage />
+            </v-container>
+          </v-main>
+        </v-layout>
+      </v-app>
+    </v-theme-provider>
+  </ColorScheme>
 </template>
