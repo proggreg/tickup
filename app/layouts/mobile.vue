6c6466727724ef6bb96188e21ff74fb4b1f0e231
Greg Field <greg.field1992@gmail.com>
2026-06-27 15:21:23 +0100

    remove 0 padding

--- app/layouts/mobile.vue
+++ app/layouts/mobile.vue
@@ -3,22 +3,16 @@ const colorMode = useColorMode();
 </script>
 
 <template>
-    <ColorScheme>
-        <v-theme-provider
-            with-background
-            :theme="colorMode.preference"
-        >
-            <v-app>
-                <v-main>
-                    <v-container
-                        class="pa-0"
-                        fluid
-                    >
-                        <NuxtPage />
-                        <AppNavMobile />
-                    </v-container>
-                </v-main>
-            </v-app>
-        </v-theme-provider>
-    </ColorScheme>
+  <ColorScheme>
+    <v-theme-provider with-background :theme="colorMode.preference">
+      <v-app>
+        <v-main>
+          <v-container fluid>
+            <NuxtPage />
+            <AppNavMobile />
+          </v-container>
+        </v-main>
+      </v-app>
+    </v-theme-provider>
+  </ColorScheme>
 </template>
