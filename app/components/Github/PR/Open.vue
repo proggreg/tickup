b48030f1d4d8775469c4bfa863fb4883868e77cd
Greg Field <greg.field1992@gmail.com>
2026-06-28 17:31:52 +0100

    style(github): make PR open button green

--- app/components/Github/PR/Open.vue
+++ app/components/Github/PR/Open.vue
@@ -7,6 +7,7 @@ const listStore = useListsStore();
         v-if="listStore.currentTodo.githubPrLink"
         size="small"
         icon="mdi-source-pull"
+        color="green"
         @click.stop="navigateTo(listStore.currentTodo.githubPrLink, { open: { target: '_blank' } })"
     />
 </template>
