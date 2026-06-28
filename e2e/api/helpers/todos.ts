5eac4f0c2c399e3f8646b9f0af978883e7781467
Greg Field <greg.field1992@gmail.com>
2026-06-28 15:26:19 +0100

    add type

--- e2e/api/helpers/todos.ts
+++ e2e/api/helpers/todos.ts
@@ -1,17 +1,21 @@
-import { createNewTodoState } from '../../../app/stores/helpers';
-import type { Todo } from '../../../index';
+import { createNewTodoState } from "../../../app/stores/helpers";
+import type { Todo } from "../../../index";
+import { APIRequestContext } from "@playwright/test";
 
-export async function createTodo(request, todo: {
+export async function createTodo(
+  request: APIRequestContext,
+  todo: {
     name: string;
     dueDate?: Date;
     listId?: string;
     parentId?: string;
-}): Promise<Todo> {
-    const payload: Todo = { ...createNewTodoState(), ...todo };
+  },
+): Promise<Todo> {
+  const payload: Todo = { ...createNewTodoState(), ...todo };
 
-    const response = await request.post('/api/todo', {
-        data: payload,
-    });
+  const response = await request.post("/api/todo", {
+    data: payload,
+  });
 
-    return await response.json();
+  return await response.json();
 }
