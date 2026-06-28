143caa2ce05136de529147e416e38bf632bbbede
Greg Field <greg.field1992@gmail.com>
2026-06-28 14:07:26 +0100

    fix tasks being cutoff

--- app/components/Board/index.vue
+++ app/components/Board/index.vue
@@ -3,15 +3,8 @@ const statusStore = useSettingsStore();
 </script>
 
 <template>
-    <div
-        class="overflow-x-auto overflow-y-hidden pa-1 pb-6"
-        style="height: calc(100vh - 180px)"
-    >
-        <v-row
-            no-gutters
-            class="flex-nowrap align-start"
-            style="min-width: max-content; height: 100%"
-        >
+    <div class="overflow-x-auto pa-1 pb-6">
+        <v-row no-gutters class="flex-nowrap align-start" style="min-width: max-content">
             <v-col
                 v-for="status in statusStore.statuses"
                 :key="status.name"
