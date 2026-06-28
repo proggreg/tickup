ea231c27055295a634000a4e3d547a83507bed5e
Greg Field <greg.field1992@gmail.com>
2026-06-28 15:13:43 +0100

    add failing test

--- e2e/api/todos/create-todo.spec.ts
+++ e2e/api/todos/create-todo.spec.ts
@@ -1,35 +1,50 @@
-import { test, expect } from '@playwright/test';
-import { v4 as uuidv4 } from 'uuid';
-import { createTodo } from '../helpers/todos';
-import { createList } from '../helpers/lists';
+import { test, expect } from "@playwright/test";
+import { v4 as uuidv4 } from "uuid";
+import { createTodo } from "../helpers/todos";
+import { createList } from "../helpers/lists";
 
-test.describe('Todos', () => {
-    test('should create a todo without a list', async ({ request }) => {
-        const testId = uuidv4();
+test.describe("Todos", () => {
+  test("should create a todo without a list", async ({ request }) => {
+    const testId = uuidv4();
 
-        const todoName = `Todo in a list ${testId}`;
-        const todo = await createTodo(request, {
-            name: todoName,
-        });
-
-        expect(todo).toBeTruthy();
-        expect(todo.id).toBeTruthy();
+    const todoName = `Todo in a list ${testId}`;
+    const todo = await createTodo(request, {
+      name: todoName,
     });
 
-    test('should create a todo within a list', async ({ request }) => {
-        const testId = uuidv4();
+    expect(todo).toBeTruthy();
+    expect(todo.id).toBeTruthy();
+  });
 
-        const list = await createList(request, {
-            name: `Test List ${testId}`,
-        });
+  test("should create a todo within a list", async ({ request }) => {
+    const testId = uuidv4();
 
-        const todoName = `Todo in a list ${testId}`;
-        const todo = await createTodo(request, {
-            name: todoName,
-            listId: list.id,
-        });
-
-        expect(todo).toBeTruthy();
-        expect(todo.id).toBeTruthy();
+    const list = await createList(request, {
+      name: `Test List ${testId}`,
     });
+
+    const todoName = `Todo in a list ${testId}`;
+    const todo = await createTodo(request, {
+      name: todoName,
+      listId: list.id,
+    });
+
+    expect(todo).toBeTruthy();
+    expect(todo.id).toBeTruthy();
+  });
+
+  test("should default status to open if no status is given", async ({
+    request,
+  }) => {
+    const testId = uuidv4();
+
+    const todoName = `Todo without a status ${testId}`;
+    const todo = await createTodo(request, {
+      name: todoName,
+    });
+
+    expect(todo).toBeTruthy();
+    expect(todo.id).toBeTruthy();
+    expect(todo.status).toEqual("Open");
+  });
 });
